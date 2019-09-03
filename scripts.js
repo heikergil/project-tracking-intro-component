0d1e345e29448ffac871da5f6edf0c7dbe9b1929
Heiker Gil <gilheyker@gmail.com>
2019-09-03 18:56:23 -0500

    some css

--- scripts.js
+++ scripts.js
@@ -4,6 +4,13 @@ const links = document.querySelectorAll(".links");
 
 
 hamburguer.addEventListener("click", () => {
+
+    if (nav_links.classList.contains('hidden')) {
     nav_links.classList.remove("hidden");
+    nav_links.classList.add("flex");
+    } else {
+        nav_links.classList.remove("flex");
+        nav_links.classList.add("hidden");
+    } 
 })
 
